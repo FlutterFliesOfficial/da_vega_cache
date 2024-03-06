7b811088a7bef1a4c01e41ac5eb53c26a38335a1
Manthan997 <manthanparsekar997@gmail.com>
2024-03-06 15:37:08 +0530

    Revert "Merge branch 'main' of https://github.com/FlutterFliesOfficial/da_vega_cache"
    This reverts commit 5464e64b3d82e2dfafdbd1b98fd2dbc97960717d, reversing
    changes made to 574449c70c052c5cb72f481dfd41d90649cb23c2.

--- lib/widgets/app_assets.dart
+++
@@ -1,7 +0,0 @@
-class AppAssets {
-  static const String kOnboarding1 = 'assets/images/onboarding_1.png';
-  static const String kOnboarding2 = 'assets/images/onboarding_2.png';
-  static const String kOnboarding3 = 'assets/images/onboarding_3.png';
-  static const String kFaceBook = 'assets/icons/facebook.svg';
-  static const String kGoogle = 'assets/icons/google.svg';
-}
